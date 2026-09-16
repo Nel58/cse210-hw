d23303e8e5ddd515deeb5799058ae225a8783d79
Marshall Nelson <nel22011@byui.edu>
2026-09-16 10:00:39 -0600

    Created and Finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+        Console.Write("Please Enter your first name:");
+        firstName = Console.ReadLine();
+        Console.Write("Please Enter your last name");
+        lastName = Console.ReadLine();
+        Console.Write ($"{firstName} {lastName}");
     }
 }
