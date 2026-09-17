d701b8b4ffe2f5fdfafd3db4d54da839538f0bbe
Marshall Nelson <nel22011@byui.edu>
2026-09-17 08:04:42 -0600

    I was able to get it to do all of the 3 functions as outlined in assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,94 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    string userinput;
+    int percentage;
+    string letter;
+    bool Pass;
+     Console.Write("What is your percentage in your class?: ");
+     userinput = Console.ReadLine();
+      percentage  =int.Parse(userinput);
+    Console.WriteLine($"Your Percentage in your class is {percentage}%");
+    if (percentage <= 93 && percentage >= 100)
+        {
+            //Console.Write("A");
+            letter = "A";
+            Pass =true;
+        }
+    else if  (percentage <= 90 && percentage >= 92.99)
+        {
+            //Console.Write("A-");
+            letter = "A-";
+            Pass =true;
+        }
+     else if  (percentage <= 87 && percentage >= 89.99)
+        {
+            //Console.Write("B+");
+            letter = "B+";
+            Pass =true;
+        }
+    else if  (percentage <= 83 && percentage >= 86.99)
+        {
+            //Console.Write("B");
+             letter = "B";
+             Pass =true;
+        }
+        else if  (percentage <= 80 && percentage >= 82.99)
+        {
+            //Console.Write("B-");
+            letter = "B-";
+            Pass =true;
+        }
+            else if  (percentage <= 77 && percentage >= 79.99)
+        {
+            //Console.Write("C+");
+            letter = "C+";
+            Pass =true;
+        }
+            else if  (percentage <= 73 && percentage >= 76.99)
+        {
+            //Console.Write("C");
+            letter = "C";
+            Pass =true;
+        }
+            else if  (percentage <= 70 && percentage >= 72.99)
+        {
+            //Console.Write("C-");
+            letter = "C=";
+            Pass =true;
+        }
+            else if  (percentage <= 67 && percentage >= 69.99)
+        {
+            //Console.Write("D+");
+            letter = "D+";
+             Pass = false;
+        }
+            else if  (percentage <= 65 && percentage >= 66.99)
+        {
+            //Console.Write("D");
+            letter = "D";
+             Pass = false;
+        }
+        else
+        {
+            //Console.Write("F");
+            letter = "F";
+            Pass = false;
+        }
+        Console.WriteLine($"Your Letter grade from the course is {letter}.");
+        if (Pass == true)
+        {
+            Console.WriteLine($"You passed your course congrats!!");
+        }
+        else
+        {
+            Console.WriteLine($" You failed your course, next time put in more effort!");
+        }
+
+    
     }
 }
